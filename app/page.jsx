4d4d8f35f6cd372be64d7ae0bf9d17a840571633
Raushan-Kumar-Gupta <raushankalwar98242@gmail.com>
2025-07-04 01:02:18 +0530

    Add HeroSection component and grid background styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
+import HeroSection from "@/components/Hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive">Destructive</Button>
-
+      <div className="grid-background"></div>
+      <HeroSection/>
     </div>
     
   );

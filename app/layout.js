173a717e4079fa61c20670ae27dada04eba32a82
Raushan-Kumar-Gupta <raushankalwar98242@gmail.com>
2025-07-05 23:50:24 +0530

    add onboarding flow, form validation, toaster, 404 page, useFetch hook

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
               {/* header */}
               <Header/>
               <main className="min-h-screen">{children}</main>
+              <Toaster richColors />
               {/* footer */}
               <footer className="bg-muted/50 py-12">
                 <div className="container mx-auto px-4 text-center text-gray-200">
